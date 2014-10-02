818f0c991d067f6a066c21de8145b18438e354a6
Charlie FG <charlie@carrotsrc.org>
2014-10-02 15:03:59 +0100

    Added optional template for roles

---
+++ base/opt/configs/Roles.php
@@ -0,0 +1,9 @@
+<?php
+	/*
+	This acts as a template for roles.
+	Assuming a 64bit system, you can have
+	64 types of role, which can be mixed.
+	Role ID is power of two
+	*/
+	define('PX_ANON', 0);
+	define('PX_ADMIN', 1);

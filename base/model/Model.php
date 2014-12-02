ea361dad20f41741a8708ee3ac4facf5ccb13464
Charlie FG <charlie@carrotsrc.org>
2014-12-02 17:37:47 +0000

    Removed redundent comments

--- base/model/Model.php
+++ base/model/Model.php
@@ -9,10 +9,6 @@
 	 * These represent things like users or interfaces for algorithms.
 	 * They can be quickly loaded from ControlType flavoured objects.
 	 * They allow controllers to deal with the data.
-	 * 
-	 *
-	 * This class also acts as the loader for instances of itself
-	 * using the static load method.
 	 */
 	abstract class Model extends PXBase {
 

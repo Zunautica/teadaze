5fe753406c3293b9082eea7df56ec2a3d3e73299
Charlie FG <charlie@carrotsrc.org>
2014-12-02 17:36:17 +0000

    Moved database related stuff out of the base Model object

--- base/model/Model.php
+++ base/model/Model.php
@@ -3,8 +3,6 @@
 *  Distributed under the MIT License (http://opensource.org/licenses/MIT)
 */
 
-	include('TableModel.php');
-
 	/**
 	 * A model for representing any form of data or business logic
 	 *
@@ -12,54 +10,11 @@
 	 * They can be quickly loaded from ControlType flavoured objects.
 	 * They allow controllers to deal with the data.
 	 * 
-	 * Models come with database access as standard and allow quick
-	 * communication with the database through a Table Model. You can
-	 * also dereference the table object when calling the method; this
-	 * can help for readablity:
-	 *
-	 * $this->table('users')->select('*');
 	 *
 	 * This class also acts as the loader for instances of itself
 	 * using the static load method.
 	 */
-	abstract class Model extends DBAccessor implements DBInterface {
+	abstract class Model extends PXBase {
 
-		/**
-		 * A method for generating a new isntance of a table model
-		 *
-		 * This can be used to quickly send a table orientated
-		 * statement to the database. You can dereference the 
-		 * table object for readability.
-		 *
-		 * @method table(string $table)
-		 * @param string $table The name of the table to assign to the Table Model
-		 * @access protected
-		 * @return PTableModel The newly instantiated Table Model
-		 */
-		protected function table($table)
-		{
-			return new PXTableModel($this->db, $table);
-		}
 
-		/**
-		 * Send a straight MySQL query to the database
-		 *
-		 * Use this if you just want to send a statement.
-		 * It will pass back an associative array or boolean
-		 * depending on the statement and result.
-		 *
-		 * This method is *dangerous* - it does not provide
-		 * any sanitation
-		 *
-		 * @method query(string $sql)
-		 * @param string $sql The SQL statement to send
-		 * @access public
-		 * @return array|boolean The result of the query
-		 */
-		protected function query($sql)
-		{
-			return $this->db->query($sql);
-		}
-
-		public function setDatabase($database){}
 	}
